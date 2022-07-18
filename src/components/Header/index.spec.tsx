abef76f433e81ce3d515dbdcba25a409987277aa
Diego Dias <diegodias@Diegos-MacBook-Pro.local>
2022-07-18 12:52:34 -0300

    Adding jest and testing library

---
+++ src/components/Header/index.spec.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Header } from ".";
+
+describe("FadeInEntrance component", () => {
+  let rootElement: any = null;
+
+  beforeEach(() => {
+    rootElement = document.createElement("div");
+    document.body.appendChild(rootElement);
+  });
+
+  afterEach(() => {
+    rootElement && document.body.removeChild(rootElement);
+    rootElement = null;
+  });
+
+  it("renders as expected", () => {
+    const props = {};
+
+    const { container } = render(<Header />, rootElement);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
